dcfd9551dcddd510200708ab48e5dfc13c6ade22
ttimbers <tiffany.timbers@stat.ubc.ca>
2020-03-12 11:26:14 -0700

    initial commit

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use foocat5 in a project::
+
+    from foocat5 import foocat5
---
+++ tests/test_foocat5.py
@@ -0,0 +1,9 @@
+from foocat5 import foocat5
+import pandas as pd
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((foocat5.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((foocat5.catbind(a, b)).categories == ["but", "character", "counts",
+            "eyeballs", "hits", "integer", "where it", "your"]).all()
